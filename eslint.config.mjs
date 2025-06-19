6296652137bba3fe16a38035ed9eeb4b2be64e92
Marcos Rodríguez <marcosrodriguezroman700.02@gmail.com>
2025-06-19 21:40:34 +0200

    chore: modify ESLint rules to support TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,38 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-
+import tseslint from "typescript-eslint";
+import path from "node:path";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,tsx}"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: path.resolve('tsconfig.lib.json'),
+        tsconfigRootDir: path.resolve('./'),
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin
+    },
+    rules: {
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn"
+    }
+  }
 ]);
