26a5f5f867d5f2941d6034ab42e0aeff743bff78
Marcos Rodríguez <marcosrodriguezroman700.02@gmail.com>
2025-06-25 17:06:35 +0200

    feat: add vertical-timeline component

---
+++ src/lib/types/timeline-data.ts
@@ -0,0 +1,5 @@
+export interface TimelineData {
+    title: string,
+    subtitle: string
+    description: string
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -14,6 +14,12 @@ export * from './lib/tables/table/table.component';
 export * from './lib/icons/icon-helper/icon-helper.component'
 export * from './lib/icons/icon-warning/icon-warning.component'
 
+/** Cards components */
+export * from './lib/cards/simple-card/simple-card.component'
+export * from './lib/cards/card/card.component'
+
+/** Timelines component */
+export * from './lib/timelines/vertical-timeline/vertical-timeline.component'
 
 /** Interfaces */
 export * from './lib/types/table-data'
